0dedcfab96a3e0f808e980ecdbe9caf45a0f1a26
Cheong <cheong@redbearlab.com>
2014-05-09 15:23:45 +0800

    Updated .bat for generating service.h file.

--- Arduino/libraries/RBL_nRF8001/run_me_compile_xml_to_nRF8001_setup.bat
+++ Arduino/libraries/RBL_nRF8001/run_me_compile_xml_to_nRF8001_setup.bat
@@ -2,4 +2,4 @@ del services.h
 del services_lock.h
 del ublue_setup.gen.out.txt
 
-"%NRFGOSTUDIOPATH%"\nrfgostudio.exe -nrf8001 -g RBL_BLEShield.xml -codeGenVersion 1 -o .
+"%NRFGOSTUDIOPATH%"\nrfgostudio.exe -nrf8001 -g RBL_nRF8001.xml -codeGenVersion 1 -o .
